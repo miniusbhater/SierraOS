7c76bf7a1e2e234c96477361728bacbcace57678
Rose <miniusbhater@gmail.com>
2025-08-27 14:20:51 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SierraOS
+Take That Terry Davis
---
+++ SierraOS/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using System.Threading;
+namespace SierraOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to SierraOS");
+            Console.Beep();
+            Console.Clear();
+        }
+
+        protected override void Run()
+        {          
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+            
+        }
+
+    }
+}
