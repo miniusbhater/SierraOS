2e174fa5f852bfa817733561824995a9f8ce862d
Rose <miniusbhater@gmail.com>
2025-08-27 14:38:37 +0100

    Do the SierraOS ascii thing

--- SierraOS/Kernel.cs
+++ SierraOS/Kernel.cs
@@ -10,10 +10,14 @@ namespace SierraOS
 
         protected override void BeforeRun()
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Welcome to SierraOS");
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine("\r\n\r\n ____  _                      ___  ____  \r\n/ ___|(_) ___ _ __ _ __ __ _ / _ \\/ ___| \r\n\\___ \\| |/ _ \\ '__| '__/ _` | | | \\___ \\ \r\n ___) | |  __/ |  | | | (_| | |_| |___) |\r\n|____/|_|\\___|_|  |_|  \\__,_|\\___/|____/ \r\n\r\n");
             Console.Beep();
+            Thread.Sleep(1500);            
             Console.Clear();
         }
 
