491b89d841435374d2f2cf53ab30dba1402ebef7
Adam1000 <azeiser08@outlook.com>
2025-08-29 05:05:46 -0400

    started working on stuff for applications

---
+++ SierraOS/Applications/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SierraOS.Applications
+{
+    public class Application : IApplication
+    {
+        string IApplication.Name { get; set; }
+
+        string IApplication.Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ SierraOS/Applications/IApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SierraOS.Applications
+{
+    public interface IApplication
+    {
+        string Name { get; set; }
+
+        string Description { get; set; }
+
+        //int Version { get; set; }
+    }
+}
