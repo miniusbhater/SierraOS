aa7f7a1789d7ee9285b9b830c4732ea75788e44d
Adam1000 <azeiser08@outlook.com>
2025-09-09 11:10:27 -0400

    starting work on gui

---
+++ SierraOS/GUI/GUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+
+namespace SierraOS.GUI
+{
+    public class GUI
+    {
+    }
+}
