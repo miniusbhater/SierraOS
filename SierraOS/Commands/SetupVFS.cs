cda0584ec982dada556d042478903ceb6a398961
Adam1000 <azeiser08@outlook.com>
2025-10-08 10:43:04 -0400

    remade filesystem setup

--- SierraOS/Commands/SetupVFS.cs
+++ SierraOS/Commands/SetupVFS.cs
@@ -1,15 +1,23 @@
 ﻿using Cosmos.System.FileSystem.VFS;
 using System;
+using Sys = Cosmos.System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Cosmos.System.FileSystem;
+using SierraOS;
 
 namespace SierraOS.Commands
 {
     public class SetupVFS : Command
     {
+        Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+
+        public static long collectingDriveSpace;
+        public static string collectingSystemFile;
+
+        public static string collectingSystemLabel;
         public SetupVFS(string name) : base(name) { }
 
         public override string execute(string[] args)
@@ -35,22 +43,33 @@ namespace SierraOS.Commands
 
         public void VFS()
         {
-
             Console.WriteLine("Setting up VFS");
             try
             {
 
-                var vfs = new Cosmos.System.FileSystem.CosmosVFS();
+                /*var vfs = new Cosmos.System.FileSystem.CosmosVFS();
                 VFSManager.RegisterVFS(vfs);
                 Console.WriteLine("VFS setup");
                 var free = vfs.GetAvailableFreeSpace(@"0:\");
                 var total = vfs.GetTotalSize(@"0:\");
                 Console.WriteLine($"Disk Size: {total / 1024 / 1024} MB");
-                Console.WriteLine($"Free Space: {free / 1024 / 1024} MB");
+                Console.WriteLine($"Free Space: {free / 1024 / 1024} MB");*/
+                Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+                Console.WriteLine("File System Successfully Enabled", Kernel.Status.OK);
+                fs.Initialize(true);
+                long collecting_drive_space1 = fs.GetAvailableFreeSpace("0:\\");
+                string collecting_system_file1 = fs.GetFileSystemType("0:\\");
+
+                var collecting_system_label1 = fs.GetFileSystemLabel("0:\\");
+
+                collectingDriveSpace = collecting_drive_space1;
+                collectingSystemFile = collecting_system_file1;
+                collectingSystemLabel = collecting_system_label1;
 
             }
             catch (Exception ex)
             {
+                //Console.WriteLine("Enable File System Fail Successfully", Kernel.Status.ERROR);
                 Console.WriteLine($"Error : {ex}");
             }
 
